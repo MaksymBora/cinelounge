dffd44cd3196e0303fb787796db4b7a9014f401f
MaksymBora <maxboraod@gmail.com>
2023-10-15 18:03:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## cinelounge
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './components/App';
+import { GlobalStyle } from './styleTheme/GlobalStyle';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/cinelounge">
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
