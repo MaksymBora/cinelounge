c5338ee4a7348a68beaef058aeb934d8242f8f81
MaksymBora <maxboraod@gmail.com>
2023-10-21 21:28:47 +0200

    update scroll cast

--- src/components/MovieInfo/MovieCastCard.tsx
+++ src/components/MovieInfo/MovieCastCard.tsx
@@ -16,12 +16,15 @@ export const MovieCastCard = ({
   return (
     <li
       className="w-[138px] bg-bgCard rounded-cardBr shadow-castShadow border
-	 border-black [&>img]:w-full [&>img]:flex [&>img]:rounded-s-sm"
+	 border-black"
     >
       <Link to="/">
-        {' '}
         {/* Person ID */}
-        <img src={`${imageBase}w342${profile_path}`} alt={name} />
+        <img
+          src={`${imageBase}w342${profile_path}`}
+          alt={name}
+          className="w-full flex rounded-s-sm"
+        />
       </Link>
       <div className="py-[13px] px-[11px]">
         <h3 className="text-sm">{name}</h3>
