098bcbd3a0fa59be11fa78c33a5ed3876ef7aa31
MaksymBora <maxboraod@gmail.com>
2023-11-29 15:20:06 +0100

    update footer

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -2,9 +2,9 @@ import logo from '@/images/logo1.svg';
 
 export const Footer = (): JSX.Element => {
   return (
-    <footer className="h-[70px] bg-navFooter shadow-navShadow">
+    <footer className="h-[70px] bg-[#f5f5f5] dark:bg-navFooter shadow-navShadow">
       <div className="flex justify-center items-center max-w-xxl h-full my-0 mx-auto text-white leading-normal">
-        <p className="text-mainTextColo tracking-[0.5px] text-base">
+        <p className="text-black dark:text-mainTextColo tracking-[0.5px] text-base">
           Data provided by:
         </p>
         <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
