71d3797e441ea1676f667e46efc914c0c520736b
MaksymBora <maxboraod@gmail.com>
2023-12-03 14:05:56 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite';
+import * as vite from 'vite';
 import { fileURLToPath, URL } from 'url';
 import react from '@vitejs/plugin-react';
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
+export default vite.defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
