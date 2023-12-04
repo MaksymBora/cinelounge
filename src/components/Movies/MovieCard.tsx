63ee9e64864e4913bd1653d560c14b256856d131
MaksymBora <maxboraod@gmail.com>
2023-12-04 22:41:10 +0100

    added img plug

--- src/components/Movies/MovieCard.tsx
+++ src/components/Movies/MovieCard.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FiPercent } from 'react-icons/fi';
 import { imageBase } from '@/service/imagePath';
 import { colorPercentage, formatDate } from '@/utilities/utilities';
+import plug from '../../assets/plug.jpg';
 
 interface MovieCardProps {
   movieData: {
@@ -16,38 +17,42 @@ interface MovieCardProps {
 
 export const MovieCard: FC<MovieCardProps> = ({
   movieData: { poster_path, title, release_date, vote_average, id },
-}) => (
-  <div className="bg-white dark:bg-bgCard flex flex-col rounded-cardBr shadow-cardShadow">
-    <Link to={`/${id}`} className="relative pt-150">
-      <img
-        src={`${imageBase}w500${poster_path}`}
-        alt={title}
-        loading="lazy"
-        className="absolute top-0 left-0 rounded-t-cardBr"
-      />
-    </Link>
-    <div className="p-details min-h-100 flex flex-col relative">
-      <h2 className="font-semibold text-base text-black dark:text-mainTextColo">
-        {title}
-      </h2>
-      <p className="text-13 mt-1.5 text-[#3c3c3c] dark:text-secondaryText">
-        {release_date
-          ? formatDate(release_date.replace(/-/g, '/'), 'short')
-          : ''}
-      </p>
-      <div
-        style={{
-          border: `3px solid ${
-            vote_average ? colorPercentage(vote_average / 10) : '#777'
-          }`,
-        }}
-        className="absolute top-rateT left-rateL flex justify-center items-center bg-rateBg w-10 h-10 rounded-rating text-xs text-white"
-      >
-        <p>{vote_average ? +vote_average.toFixed(1) * 10 : 'NR'}</p>
-        {vote_average ? (
-          <FiPercent className="text-8 translate-x-[1px] translate-y-[-3px]" />
-        ) : null}
+}) => {
+  const image = poster_path ? `${imageBase}w500${poster_path}` : plug;
+
+  return (
+    <div className="bg-white dark:bg-bgCard flex flex-col rounded-cardBr shadow-cardShadow">
+      <Link to={`/${id}`} className="relative pt-150">
+        <img
+          src={image}
+          alt={title}
+          loading="lazy"
+          className="absolute top-0 left-0 rounded-t-cardBr"
+        />
+      </Link>
+      <div className="p-details min-h-100 flex flex-col relative">
+        <h2 className="font-semibold text-base text-black dark:text-mainTextColo">
+          {title}
+        </h2>
+        <p className="text-13 mt-1.5 text-[#3c3c3c] dark:text-secondaryText">
+          {release_date
+            ? formatDate(release_date.replace(/-/g, '/'), 'short')
+            : ''}
+        </p>
+        <div
+          style={{
+            border: `3px solid ${
+              vote_average ? colorPercentage(vote_average / 10) : '#777'
+            }`,
+          }}
+          className="absolute top-rateT left-rateL flex justify-center items-center bg-rateBg w-10 h-10 rounded-rating text-xs text-white"
+        >
+          <p>{vote_average ? +vote_average.toFixed(1) * 10 : 'NR'}</p>
+          {vote_average ? (
+            <FiPercent className="text-8 translate-x-[1px] translate-y-[-3px]" />
+          ) : null}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
--- src/components/Shows/ShowsCard.tsx
+++ src/components/Shows/ShowsCard.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FiPercent } from 'react-icons/fi';
 import { colorPercentage, formatDate } from '@/utilities/utilities';
 import { imageBase } from '@/service/imagePath';
+import plug from '../../assets/plug.jpg';
 
 interface ShowsCardProps {
   showsData: {
@@ -16,38 +17,41 @@ interface ShowsCardProps {
 
 export const ShowsCard: FC<ShowsCardProps> = ({
   showsData: { poster_path, name, first_air_date, vote_average, id },
-}) => (
-  <div className="bg-white dark:bg-bgCard flex flex-col rounded-cardBr shadow-cardShadow">
-    <Link to={`/shows/${id}`} className="relative pt-150">
-      <img
-        src={`${imageBase}w500${poster_path}`}
-        alt={name}
-        loading="lazy"
-        className="absolute top-0 left-0 rounded-t-cardBr"
-      />
-    </Link>
-    <div className="p-details min-h-100 flex flex-col relative">
-      <h2 className="font-semibold text-base text-black dark:text-mainTextColo">
-        {name}
-      </h2>
-      <p className="text-13 mt-1.5 text-[#3c3c3c] dark:text-secondaryText">
-        {first_air_date
-          ? formatDate(first_air_date.replace(/-/g, '/'), 'short')
-          : ''}
-      </p>
-      <div
-        style={{
-          border: `3px solid ${
-            vote_average ? colorPercentage(vote_average / 10) : '#777'
-          }`,
-        }}
-        className="absolute top-rateT left-rateL flex justify-center items-center bg-rateBg w-10 h-10 rounded-rating text-xs text-white"
-      >
-        <p>{vote_average ? +vote_average.toFixed(1) * 10 : 'NR'}</p>
-        {vote_average ? (
-          <FiPercent className="text-8 translate-x-[1px] translate-y-[-3px]" />
-        ) : null}
+}) => {
+  const image = poster_path ? `${imageBase}w500${poster_path}` : plug;
+  return (
+    <div className="bg-white dark:bg-bgCard flex flex-col rounded-cardBr shadow-cardShadow">
+      <Link to={`/shows/${id}`} className="relative pt-150">
+        <img
+          src={image}
+          alt={name}
+          loading="lazy"
+          className="absolute top-0 left-0 rounded-t-cardBr"
+        />
+      </Link>
+      <div className="p-details min-h-100 flex flex-col relative">
+        <h2 className="font-semibold text-base text-black dark:text-mainTextColo">
+          {name}
+        </h2>
+        <p className="text-13 mt-1.5 text-[#3c3c3c] dark:text-secondaryText">
+          {first_air_date
+            ? formatDate(first_air_date.replace(/-/g, '/'), 'short')
+            : ''}
+        </p>
+        <div
+          style={{
+            border: `3px solid ${
+              vote_average ? colorPercentage(vote_average / 10) : '#777'
+            }`,
+          }}
+          className="absolute top-rateT left-rateL flex justify-center items-center bg-rateBg w-10 h-10 rounded-rating text-xs text-white"
+        >
+          <p>{vote_average ? +vote_average.toFixed(1) * 10 : 'NR'}</p>
+          {vote_average ? (
+            <FiPercent className="text-8 translate-x-[1px] translate-y-[-3px]" />
+          ) : null}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
